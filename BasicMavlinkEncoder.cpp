a1adee3978f57d130fb511aa86778d951e05961e
ydzulynsky <yarema.dzulynsky@gmail.com>
2023-07-29 11:30:49 -0400

    Added Comments

--- BasicMavlinkEncoder.cpp
+++ BasicMavlinkEncoder.cpp
@@ -21,11 +21,21 @@ void packIntoVector(size_t bufferLen, std::vector<uint8_t> &vectorBuffer, const 
 
 std::vector<uint8_t> BasicMavlinkEncoder::findPackingFunction(IncomingData &data) {
 
+    // Create a vector to store the encoded message
     std::vector<uint8_t> vectorBuffer;
+
+    // Create a temporary variable to store the size of the encoded message in the buffer
     std::size_t tempSize;
 
+    // Create a temporary buffer to store the encoded message before adding it to the vector.
     auto *tempBuffer = new uint8_t[MAVLINK_MAX_PACKET_LEN];
 
+    /**
+     * This is currently very primitive and only checks if the data is initialized not if it is valid.
+     * It also only (Barely) supports the global_position_int and attitude messages. To create a more
+     * robust system, I need to know exactly what kinds of messages I will be receiving as this part
+     * of the code is very specific to the messages I am receiving and involves a lot of hard coding.
+     */
     if(data.isLatitudeInitialized || data.isLongitudeInitialized || data.isAltitudeInitialized) {
         tempSize = ENCODE_MESSAGE(global_position_int, 0, data.latitude, data.longitude, data.altitude, 0, data.vx, data.vy, data.vz, 0)(currentMessage, tempBuffer);
         packIntoVector(tempSize, vectorBuffer, tempBuffer);
@@ -35,6 +45,7 @@ std::vector<uint8_t> BasicMavlinkEncoder::findPackingFunction(IncomingData &data
         packIntoVector(tempSize, vectorBuffer, tempBuffer);
     }
 
+    // Delete the temporary buffer
     delete[] tempBuffer;
 
     return vectorBuffer;
